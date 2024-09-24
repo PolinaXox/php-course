e65cd5e9723e4dcf1bee28f61f3f88ef0f4388d8
Polina Xox <polina_xoxlova@ukr.net>
2024-09-24 20:24:46 +0300

    redone by notes dated 23.09.2024

--- 1. Basics/task_2/hw2.php
+++ 1. Basics/task_2/hw2.php
@@ -1,11 +1,15 @@
 <?php
+
 /**
- * @return string the input request example as a string
+ * Returns the input request example as a string
+ * 
+ * @return string 
  */
-function readHttpLikeInput() : string {
+function readHttpLikeInput() {
     $f = fopen( 'php://stdin', 'r' );
     $store = "";
     $toread = 0;
+    
     while( $line = fgets( $f ) ) {
         $store .= preg_replace("/\r/", "", $line);
 
@@ -30,35 +34,42 @@ function readHttpLikeInput() : string {
 $contents = readHttpLikeInput();
 
 /**
- * @param string $string : input request sample as a string
+ * Gets input request sample as a string and returns array of the needed request parts
+ * 
+ * @param string $string
  *
- * @return array of the needed request parts
+ * @return array
  */
 function parseTcpStringAsHttpRequest($string) : array {
-    $strAsArr = explode("\n", $string);
+    $strToParse = getStrToParse($string);
+    $strAsArr = cleanArray(explode(PHP_EOL, $strToParse));
+    
+    // parse and delete method and uri
     $methodAndURI = explode(" ", array_shift($strAsArr));
-    $headers = [];
-    $body = NULL;
-
-    // if the request has headers store each header data as
-    // $headers[headerName] = headerValue
-    if($strAsArr) {
-        $i = 0;
-        while($strAsArr[$i]) {
-            $ind = strpos($strAsArr[$i], ":");
-            $headers[trim(substr($strAsArr[$i], 0, $ind))] = trim(substr($strAsArr[$i], $ind+1));
-            $i++;
-        }
-
-        // empyty line between header(s) and body, if it is
-        $i++;
-        
-        // if the request has a body
-        if($i < count($strAsArr)) {          
-            $body = trim($strAsArr[$i]);
+    
+    // find body if it is and delete body from the array
+    $body = "";
+    $arrSize = count($strAsArr);
+    
+    if($arrSize > 0) {
+        if(hasBody($string, $strAsArr[$arrSize-1])) {
+            $body = array_pop($strAsArr);
+            $arrSize--;
         }
     }
 
+    // parse headers[]
+    $headers = [];
+    $i = 0;
+    
+    // if the request has headers store each header data as
+    // $headers[headerName] = headerValue
+    while($i < $arrSize) {
+        $ind = strpos($strAsArr[$i], ":");
+        $headers[trim(substr($strAsArr[$i], 0, $ind))] = trim(substr($strAsArr[$i], $ind+1));
+        $i++;
+    }
+    
     return array(
         "method" => trim($methodAndURI[0]),
         "uri" => trim($methodAndURI[1]),
@@ -69,3 +80,45 @@ function parseTcpStringAsHttpRequest($string) : array {
 
 $http = parseTcpStringAsHttpRequest($contents);
 echo(json_encode($http, JSON_PRETTY_PRINT));
+
+/**
+ * Gets string as it input and returns a string which let use PHP_EOL as a delimeter
+ * 
+ * @param $string
+ * 
+ * @return string
+ */
+
+function getStrToParse($string) : string {
+    $str = preg_replace("/\r/", "", $string);
+    $str = preg_replace("/\n/", PHP_EOL, $str);
+    return $str;
+}
+
+/**
+ * Removes items which are empty lines from the array
+ * 
+ * @param array $arr
+ * 
+ * @return array
+ */
+
+function cleanArray($arr) : array {
+    $resultArr = [];
+    foreach($arr as $item) {
+        if(strlen($item) > 0){
+            $resultArr[]=$item;
+        }
+    }
+    return $resultArr;
+}
+
+/**
+ * @param string $string
+ * @param string $arrLastItem
+ * 
+ * @return bool
+ */
+function hasBody($string, $arrLastItem) : bool {
+    return preg_match('/Content-Length:/', $string) || !preg_match('/:/', $arrLastItem);
+}
