1a4b9dfd1737bfbf14b8fd625a1aa43e404de164
PolinaXox <157826347+PolinaXox@users.noreply.github.com>
2024-09-25 12:59:52 +0300

    change task3 by notes dated 23.09.2024
    changed task3 by mentor's istructions and remarks dated 23.09.2024
    parcing logic has changed accordg task2
    part of parsing with ```strpos()``` has no changed because I didn't guess yet which option's better

--- 1. Basics/task_3/hw3.php
+++ 1. Basics/task_3/hw3.php
@@ -2,21 +2,28 @@
 date_default_timezone_set("Europe/Kyiv");
 
 /**
- * @return string : input request sample as a string 
+ * Returns the input request example as a string
+ * 
+ * @return string 
  */
 function readHttpLikeInput() : string {
     $f = fopen( 'php://stdin', 'r' );
     $store = "";
     $toread = 0;
+   
     while( $line = fgets( $f ) ) {
         $store .= preg_replace("/\r/", "", $line);
-        if (preg_match('/Content-Length: (\d+)/',$line,$m)) 
+        
+        if (preg_match('/Content-Length: (\d+)/',$line,$m))
             $toread=$m[1]*1; 
-        if ($line == "\r\n") 
-              break;
+        
+            if ($line == "\r\n")
+                break;
     }
+    
     if ($toread > 0) 
         $store .= fread($f, $toread);
+
     return $store;
 }
 
@@ -29,63 +36,75 @@ $contents = readHttpLikeInput();
  * @param string    $body
  */
 function outputHttpResponse($statuscode, $statusmessage, $headers, $body) : void {
-    echo "HTTP/1.1 $statuscode $statusmessage\n";
+    echo "HTTP/1.1 $statuscode $statusmessage" . PHP_EOL; 
 
     foreach($headers as $header => $value) {         
-        echo "$header : $value\n";
+        echo "$header : $value" . PHP_EOL;
     }
 
-    echo "\n$body\n"; 
+    echo PHP_EOL . $body; 
 }
 
 /**
+ * Processes the request and outputs the response
+ * 
  * @param string    $method
  * @param string    $uri
  * @param array     $headers
  * @param string    $body
  */
-function processHttpRequest($method, $uri, $headers=null, $body=null) : void {
+ function processHttpRequest($method, $uri, $headers=null, $body=null) : void {
     $statuscode = getResponseStatusCode($method, $uri);
     $statusmessage = getResponseStatusMessage($statuscode); 
-    $body = getResponseBody($statuscode, $uri);
-    $headers = array(
+    $body = getResponseBody($statuscode, $statusmessage, $uri);
+    $headers = [
         "Date" => date(DATE_RFC1123),                 
         "Server" => "Apache/2.2.14 (Win32)",
         "Content-Length" => strlen($body),
         "Connection" => "Closed",      
         "Content-Type" => "text/html; charset=utf-8",
-    );
+    ];
    
     outputHttpResponse($statuscode, $statusmessage, $headers, $body);
 }
 
 /**
-* @param string    $string : input request as a string
-*
-* @return array : the needed request parts as an array
-*/
-function parseTcpStringAsHttpRequest($string) {
-    $strAsArr = explode("\n", $string);
+ * Gets input request sample as a string and returns array of the needed request parts
+ * 
+ * @param string $string
+ *
+ * @return array
+ */
+function parseTcpStringAsHttpRequest($string) : array {
+    $strToParse = getStrToParse($string);
+    $strAsArr = cleanArray(explode(PHP_EOL, $strToParse));
+    
+    // parse and delete method and uri
     $methodAndURI = explode(" ", array_shift($strAsArr));
-    $headers = [];
-    $body = NULL;
-
-    if($strAsArr) {
-        $i = 0;
-
-        while($strAsArr[$i]) {
-            $ind = strpos($strAsArr[$i], ":");
-            $headers[trim(substr($strAsArr[$i], 0, $ind))] = trim(substr($strAsArr[$i], $ind+1));
-            $i++;
-        }
-
-        $i++;
-        
-        if($i < count($strAsArr)) {
-            $body = trim($strAsArr[$i]);
+    
+    // find body if it is and delete body from the array
+    $body = "";
+    $arrSize = count($strAsArr);
+    
+    if($arrSize > 0) {
+        if(hasBody($string, $strAsArr[$arrSize-1])) {
+            $body = array_pop($strAsArr);
+            $arrSize--;
         }
     }
 
+    // parse headers[]
+    $headers = [];
+    $i = 0;
+    
+    // if the request has headers store each header data as
+    // $headers[headerName] = headerValue
+    while($i < $arrSize) {
+        $ind = strpos($strAsArr[$i], ":");
+        $headers[trim(substr($strAsArr[$i], 0, $ind))] = trim(substr($strAsArr[$i], $ind+1));
+        $i++;
+    }
+    
     return array(
         "method" => trim($methodAndURI[0]),
         "uri" => trim($methodAndURI[1]),
@@ -98,29 +117,72 @@ $http = parseTcpStringAsHttpRequest($contents);
 processHttpRequest($http["method"], $http["uri"], $http["headers"], $http["body"]);
 
 /**
+ * Gets string as it input and returns a string which let use PHP_EOL as a delimeter
+ * 
+ * @param $string
+ * 
+ * @return string
+ */
+ function getStrToParse($string) : string {
+    $str = preg_replace("/\r/", "", $string);
+    $str = preg_replace("/\n/", PHP_EOL, $str);
+
+    return $str;
+}
+
+/**
+ * Removes items which are empty lines from the array
+ * 
+ * @param array $arr
+ * 
+ * @return array
+ */
+function cleanArray($arr) : array {
+    $resultArr = [];
+    
+    foreach($arr as $item) {
+        if(strlen($item) > 0){
+            $resultArr[]=$item;
+        }
+    }
+
+    return $resultArr;
+}
+
+/**
+ * @param string $string
+ * @param string $arrLastItem
+ * 
+ * @return bool
+ */
+function hasBody($string, $arrLastItem) : bool {
+    return preg_match('/Content-Length:/', $string) || !preg_match('/:/', $arrLastItem);
+}
+
+/**
  * @param string    $method
  * @param string    $uri
  * 
  * @return int  status code
  */
 
-// condition combination as a table in "condition_combinations of method and uri.xlsx"
+ // condition combinations as a table in "condition_combinations of method and uri.xlsx"
 function getResponseStatusCode($method, $uri) : int {
     if($method != "GET" || !preg_match('/\?nums=/i', $uri)) {
         return 400;
     }
     
-    if(preg_match('#^/sum\?nums=[\d,]+#i', $uri)) {     // integers only??? 
+    if(preg_match('#^/sum\?nums=[\d,]+#i', $uri)) { 
         return 200;
     }
 
     return 404;
 }
 
-/**
+/** 
  * @param int $statusCode
  * 
- * @return string string message
+ * @return string
  */
 function getResponseStatusMessage($statusCode) : string {
     return match($statusCode) {
@@ -133,15 +195,16 @@ function getResponseStatusMessage($statusCode) : string {
 
 /**
  * @param int       $statusCode
+ * @param string    $statusMessage
  * @param string    $uri
  * 
- * @return string   response body
+ * @return string
  */
-function getResponseBody($statusCode, $uri) : string {
+function getResponseBody($statusCode, $statusMessage, $uri) : string {
 
     // not OK
     if($statusCode != 200) {                                
-        return strtolower(getResponseStatusMessage($statusCode));
+        return strtolower($statusMessage);
     }
 
     // OK
