716e7442186e595b9b8238c57cf13b3be80a0e42
Polina Xox <polina_xoxlova@ukr.net>
2024-10-23 20:24:01 +0300

    add comments for constants in hw6_counter.php

--- 1. Basics/task_6/hw6_counter.php
+++ 1. Basics/task_6/hw6_counter.php
@@ -1,11 +1,15 @@
 <?php
+/**
+ * File name, which contains the counter
+ */
 define("FILE_NAME", "counter.txt");
+
+/**
+ * Counter start value
+ */
 define("COUNTER_START_VALUE", 0);
 
 if(!file_exists(FILE_NAME)) {
-
-    // the same as 
-    // file_put_contents(FILE_NAME, COUNTER_START_VALUE);
     $file = fopen(FILE_NAME, "w") or die("I canot create a file");
     fwrite($file, COUNTER_START_VALUE);
     fclose($file);
@@ -15,5 +19,5 @@ $counter = file_get_contents(FILE_NAME);
 echo $counter;
 $counter++;
 
-//file_put_contents() also closes files 
+//file_put_contents() also closes file 
 file_put_contents(FILE_NAME, $counter);
