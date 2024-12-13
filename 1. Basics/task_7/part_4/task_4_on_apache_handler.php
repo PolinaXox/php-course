f534b0661408d8e83ebafdad661ed39c7208d613
Polina Xox <polina_xoxlova@ukr.net>
2024-12-13 13:47:49 +0200

    add v1.0

--- 1. Basics/task_7/part_4/task_4_on_apache_handler.php
+++ 1. Basics/task_7/part_4/task_4_on_apache_handler.php
@@ -1,147 +1,161 @@
 <?php
 /**
- * Array contains data for response forming according its status code
+ * File to search logins and passwords
  */
-define('RESPONSE_DATA', [
-    '200' => ['statusMessage' => 'OK'],
-    '400' => ['statusMessage' => 'Bad Request'],
-    '404' => ['statusMessage' => 'Not Found'],
-]);
+define(constant_name: 'FILE_NAME', value: 'passwords.txt');
 
 processHttpRequest();
 
-// REQUEST PROCESS FUNCTION ///////////////////////////////////////////////////////////////////////
+// REQUEST PROCESS FUNCTIONS ///////////////////////////////////////////////////////////////////////
 /**
- * Processes http request, partly forms the response. 
- * Optionally outputs the results as HTML page.
- * 
  * @return void
  */
 function processHttpRequest() : void {
-	$nums = testInputValue($_GET['nums'] ?? false);
-	$invalidRequest = checkRequestCorrectness($nums);
-	$isHTML = preg_match('#html#', $_SERVER['HTTP_ACCEPT'] ?? '');
+	try{
+        checkRequestMethod();
+        checkRequestUri();
+        checkRequestContentTypeValue();
+
+		$loginAndPasswordPair = validateLoginAndPassword();
+        checkFileExistance(FILE_NAME);
+
+		validateUser($loginAndPasswordPair['login'], $loginAndPasswordPair['password']);
+		echo '<h1 style=\'color:green\'>FOUND</h1>';
+    }
+    catch(Exception $ex) {
+		http_response_code($ex->getCode());
+        echo '<p>' . $ex->getMessage() . '</p>';
+    };	
+}
+
+// REQUEST PROCESS FUNCTIONS. VALIDATON FUNCTIONS /////////////////////////////////////////////////
+/**
+ * @throws Exception
+ * @return void
+ */
+function checkRequestMethod() : void {
+	$methodPost = (($_SERVER['REQUEST_METHOD'] ?? null) === 'POST') ? true : false;
+
+	if($methodPost) return;
+
+    throw new Exception('Request method is invalid. The method POST is needed.', 400);
+}
+
+/**
+ * @throws Exception
+ * @return void
+ */
+function checkRequestUri() : void {
+	$correctUri = preg_match('#^/api/checkLoginAndPassword$#', $_SERVER['REQUEST_URI'] ?? '');		
+    
+	if($correctUri) return;
+
+    throw new Exception('URI is invalid.', 404);
+}
+
+/**
+ * @throws Exception
+ * @return void
+ */
+function checkRequestContentTypeValue() : void {
+	$headerContentType = apache_request_headers()['Content-Type'] ?? '';
+	$correctContentType = preg_match('#\bapplication/x-www-form-urlencoded\b#i', $headerContentType);
+
+   	if($correctContentType) return;
+    
+    throw new Exception('Header\'s \'Content-Type\' value is invalid.', 400);
+}
+
+/**
+ * @throws Exception
+ * @return array
+ */
+function validateLoginAndPassword(): array {
+	$login = makeInputDataSafe($_POST['login'] ?? false);
+
+	if($login === false){
+		throw new Exception('Login field is absent', 404);
+	}
+
+	if(empty($login)){
+		throw new Exception('Login value is absent(empty field)', 404);
+	}
+
+	$password = makeInputDataSafe($_POST['password'] ?? false);
 	
-	if($invalidRequest) {
-		http_response_code($invalidRequest['code']);
-		header('Error-Info: ' . $invalidRequest['info']);
+	if($password === false){
+		throw new Exception('Password field is absent', 404);
 	}
 	
-	$responseBody = $invalidRequest ? ($invalidRequest['info'] ?? false) : array_sum(explode(',', $nums));	
-	
-	if($responseBody && !$isHTML) echo $responseBody;
-	
-	if($isHTML) outputHTML($responseBody);
+	if(empty($password)){
+		throw new Exception('Password value is absent(empty field)', 404);
+	}
+
+	return ['login' => $login, 'password' => $password,];
 }
 
 /**
- * Checks the request's correctness.
- * If the request is correct return false.
- * If the request is incorrect returns corresponding response code and some additional information.
- *
- * @param string|bool $nums
- * @return bool|array
- */
-function checkRequestCorrectness(string|bool $nums) : bool|array {
-	$methodGet = (($_SERVER['REQUEST_METHOD'] ?? null) === 'GET') ? true : false;				// 400
-	$correctUri = preg_match('#^/sum#', $_SERVER['REQUEST_URI'] ?? '');		// 404
-	$correctNums = preg_match('#^(\d+,?)+$#', $nums);							// 400
-	
-	if(!$methodGet) return ['code' => 400, 'info' => 'Request method is invalid. The method GET is needed.'];
-
-	if(!$correctUri) return ['code' => 404, 'info' => 'Not found. URI is invalid.'];
-
-	if(!$nums) return ['code' => 400, 'info' => 'Value \'nums\' is absent.'];
-	
-	if(!$correctNums) return [ 'code' => 400, 'info' => 'Value \'nums\' is NOT comma separate numbers'];
-	
-	return false; // 200 OK	
-}
-
-// HTML OUTPUT FUNCTIONS //////////////////////////////////////////////////////////////////////////
- /**
-  * @param string $responseBody
-  * 
-  * @return void
-  */
- function outputHTML(string $responseBody) : void {
-
-	// send response for right headers list view on the HTML page 
-	flush();
-	
-	// title
-	echo '<h1>Task 3:</h1>';
-	
-	// request
-	echo '<h2>Request:</h2>';
-	outputRequest();	
-	echo '<hr style="border-top: 5px double #777">';
-	
-	// response
-	echo '<h2>Response:</h2>';
-	outputResponse($responseBody);
-	echo '<hr style="border-top: 5px double #777">';
-
-	// link 
-	echo '<br>'. mb_chr(129092) .' <a href=' . $_SERVER['HTTP_REFERER'] .'> Back to the request page</a>';
-}
-
-/**
- * Output the user`s request uncluding some decorative elements
- * 
+ * @param string $fileName
+ * @throws Exception
  * @return void
  */
-function outputRequest() : void {
-	
-	// request line
-	echo testInputValue(apache_lookup_uri($_SERVER['REQUEST_URI'])->the_request) . '<br>';
-
-	// headers
-	$headers = getallheaders();
-	array_walk($headers, 'testInputValueByReference');
-	array_walk($headers, 'printKeyAndValue');
-
-	// body
-	$body = testInputValue(file_get_contents('php://input'));
-	if($body) echo '<br>' . $body;
+function checkFileExistance(string $fileName) : void {
+    if(file_exists($fileName)) return;
+    
+    throw new Exception('File ' . $fileName . ' doesn`t exist', 500);
 }
 
 /**
- * @param bool|string $body
- * 
+ * @param string $login
+ * @param string $password
+ * @throws Exception
  * @return void
  */
-function outputResponse(bool|string $body) : void {
-	$code = http_response_code();
-	echo $_SERVER['SERVER_PROTOCOL'] . ' ' . $code . ' ' . getResponseStatusMessage($code) . '<br>';
-	
-	$headers = apache_response_headers();
-	array_walk($headers, 'printKeyAndValue');
-	echo '<br>';
-	
-	if($body) echo $body;
+function validateUser(string $login, string $password) : void {
+    $logAndPassPairsAsArr = getFileContentAsPairsArray(FILE_NAME, PHP_EOL, ':');
+
+    foreach($logAndPassPairsAsArr as $log => $pass) {
+        if($log === $login and $pass === $password) return;
+              
+		if($log === $login) {
+            throw new Exception('Password didn`t match. Fogot password?', 404);        
+        }
+    }
+
+    throw new Exception('User not found. Would you like to register?', 404);
 }
 
+
+// FILES PROCESSING FUNCTIONS ////////////////////////////////////////////////////
 /**
- * @param int $statusCode
+ * @param string $filename
+ * @param string $delim1
+ * @param string $delim2
  * 
- * @return string
+ * @return array
  */
-function getResponseStatusMessage(int $statusCode) : string {
-    return RESPONSE_DATA[$statusCode]['statusMessage'];
+function getFileContentAsPairsArray($filename, $delim1, $delim2) : array {
+    return getStringContentAsPairsArray(file_get_contents($filename), $delim1, $delim2);
 }
 
+// STRINGS PROCESSING FUNCTIONS ////////////////////////////////////////////////////
 /**
- * Outputs key and its value in a nice separate line 
- * (Uses as callback function for array_walk() for print header's/variable's names and values)
+ * @param string $str
+ * @param string $delim1
+ * @param string $delim2
  * 
- * @param string $value
- * @param string $key
- * 
- * @return void
+ * @return array
  */
-function printKeyAndValue(string &$value, string $key) : void {
-	echo '<strong>' . $key . '</strong>: ' . $value . '<br>';
+function getStringContentAsPairsArray($str, $delim1, $delim2) : array {
+    $arr = array_filter(explode($delim1, $str));
+    $pairsAsArr = [];
+    
+    foreach($arr as $pair) {
+        $p = explode($delim2, $pair);
+        $pairsAsArr += [trim($p[0])=>trim($p[1])];
+    }
+    
+    return array_filter($pairsAsArr);
 }
 
 // CHECK INPUT DATA FUNCTIONS //////////////////////////////////////////////////
@@ -153,7 +167,7 @@ function printKeyAndValue(string &$value, string $key) : void {
  * 
  * @return void
  */
- function testInputValueByReference(string &$data) : void { //
+ function makeInputDataSafeByReference(string &$data) : void { //
 	$data = trim($data);
 	$data = stripslashes($data);
 	$data = htmlspecialchars($data);
@@ -166,7 +180,30 @@ function printKeyAndValue(string &$value, string $key) : void {
  * 
  * @return string
  */
- function testInputValue(string $data) : string {
-	testInputValueByReference($data);
+ function makeInputDataSafe(string $data) : string {
+	makeInputDataSafeByReference($data);
 	return $data;
 }
+
+/*
+++ 1. переадресація на серевере + перевантаження
+
+++ 2. wrong uri -> помилка як тут
+
+++ 3. wrong Content-Type -> (розібратися як його встановлювати...) -> яка помилка??? як у попердній 4-ій
+
+++ 4. wrong method ??? (подивитися попередню роботу...)
+
+++ 5. відсутність body??? (подивитися попередню роботу...)
+
+++ 6. є пара логін + пароль -> 200 OK + body: <h1 style='color:green'>FOUND</h1>
+
+++ 7. немає пари логін + пароль -> (подивитися попередню роботу...)
+
+++ 8. Якщо файлу passwords.txt немає -> 500 Internal Server Error
+
+9. спробувати переадресацію на сторінки з помилками
+
+10. спробувати використати header Location
+
+*/
