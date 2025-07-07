4a20404e915dad27808331fcf29ee4739317b99b
Polina Xox <polina_xoxlova@ukr.net>
2025-07-07 14:01:49 +0300

    new class UserAuthService instead of two classes

--- 2. Primitive back-end/level_2_var_1/src/DomainLayer/BusinessService/UserAuthService.php
+++ 2. Primitive back-end/level_2_var_1/src/DomainLayer/BusinessService/UserAuthService.php
@@ -3,14 +3,26 @@
 namespace App\DomainLayer\BusinessService;
 
 use App\DataSourceLayer\DAO\UserDAO as UserDAO;
+use App\DomainLayer\BusinessService\ToDoListCreationService as ToDoListCreationService;
 use App\DomainLayer\Entity\User as User;
 use App\DomainLayer\Exception\AppException as AppException;
 use App\DomainLayer\Exception\AppExceptionsList as AppExceptionsList;
 use App\PresentationLayer\DataTransferObject\UserDTO as UserDTO;
 use App\PresentationLayer\InputValidator\AbsentValue as AbsentValue;
 
-class UserAuthenticationService
+class UserAuthService
 {
+    /**
+     * @param User $user
+     * @return void
+     * @throws AppException
+     */
+    public function register(User $user): void
+    {
+        new UserDAO()->save($user);
+        new ToDoListCreationService()->create($user);
+    }
+
     /**
      * @param UserDTO $userDTO
      * @return User
@@ -49,4 +61,5 @@ class UserAuthenticationService
             throw AppException::fromEnum(AppExceptionsList::PasswordMismatch);
         }
     }
+
 }
